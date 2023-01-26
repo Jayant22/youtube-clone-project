c61a33ce21e26449f3ef4d0520479127e749d264
Jayant Kodam <38130397+Jayant22@users.noreply.github.com>
2023-01-26 14:40:01 +0530

    App Commit

---
+++ src/utils/fetchFromAPI.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key':
+      'e0690a1665msh44dab378ae04d0ep18f420jsn199a4b9501b0',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
